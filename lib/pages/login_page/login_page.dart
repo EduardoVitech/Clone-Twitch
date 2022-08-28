5c6eadffd92a5b316af17b81e52ea875ef98fc2c
Mizael Eduardo <eduardo_boyfla@hotmail.com>
2022-08-28 18:29:41 -0300

    add image thumbnail

--- lib/pages/login_page/login_page.dart
+++ lib/pages/login_page/login_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:twitch_clone/pages/home_page/home_page.dart';
 import 'package:twitch_clone/resources/auth_methods/auth_methods.dart';
 import 'package:twitch_clone/utils/colors/colors.dart';
+import 'package:twitch_clone/widgets/loading_indicator/loading_indicator.dart';
 import '../../widgets/custom_button/custom_button.dart';
 import '../../widgets/custom_textfield/custom_textfield.dart';
 
@@ -17,17 +18,32 @@ class _LoginPageState extends State<LoginPage> {
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
   final AuthMethods _authMethods = AuthMethods();
+  bool _isLoading = false;
 
   loginUser() async {
+    setState(() {
+      _isLoading = true;
+    });
     bool res = await _authMethods.loginUsers(
       context,
       _emailController.text,
       _passwordController.text,
     );
 
+    setState(() {
+      _isLoading = false;
+    });
+
     if (res) {
       Navigator.pushReplacementNamed(context, HomePage.routeName);
     }
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _emailController.dispose();
+    _passwordController.dispose();
   }
 
   @override
@@ -38,48 +54,50 @@ class _LoginPageState extends State<LoginPage> {
       appBar: AppBar(
         title: const Text('Login'),
       ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 30),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(height: size.height * 0.1),
-              const Text(
-                'Email',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
+      body: _isLoading
+          ? const LoadingIndicator()
+          : SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 30),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(height: size.height * 0.1),
+                    const Text(
+                      'Email',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 8.0),
+                      child: CustomTextField(controller: _emailController),
+                    ),
+                    const SizedBox(height: 20),
+                    const Text(
+                      'Password',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 8.0),
+                      child: CustomTextField(
+                        controller: _passwordController,
+                        passwordVisible: true,
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    CustomButton(
+                      text: 'Log In',
+                      onTap: loginUser,
+                      colorButton: buttonColor,
+                      colortext: Colors.white,
+                    ),
+                  ],
                 ),
               ),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 8.0),
-                child: CustomTextField(controller: _emailController),
-              ),
-              const SizedBox(height: 20),
-              const Text(
-                'Password',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 8.0),
-                child: CustomTextField(
-                  controller: _passwordController,
-                  passwordVisible: true,
-                ),
-              ),
-              const SizedBox(height: 20),
-              CustomButton(
-                text: 'Log In',
-                onTap: loginUser,
-                colorButton: buttonColor,
-                colortext: Colors.white,
-              ),
-            ],
-          ),
-        ),
-      ),
+            ),
     );
   }
 }
--- lib/pages/signup_page/signup_pagesignup_page.dart
+++ lib/pages/signup_page/signup_pagesignup_page.dart
@@ -3,6 +3,7 @@ import 'package:twitch_clone/resources/auth_methods/auth_methods.dart';
 import 'package:twitch_clone/utils/colors/colors.dart';
 import 'package:twitch_clone/widgets/custom_button/custom_button.dart';
 import 'package:twitch_clone/widgets/custom_textfield/custom_textfield.dart';
+import 'package:twitch_clone/widgets/loading_indicator/loading_indicator.dart';
 import '../home_page/home_page.dart';
 
 class SignupPage extends StatefulWidget {
@@ -18,8 +19,13 @@ class _SignupPageState extends State<SignupPage> {
   final TextEditingController _passwordController = TextEditingController();
   final TextEditingController _usernameController = TextEditingController();
   final AuthMethods _authMethods = AuthMethods();
+  bool _isLoading = false;
 
   void signUpUser() async {
+    setState(() {
+      _isLoading = true;
+    });
+
     bool res = await _authMethods.signUpUsers(
       context,
       _emailController.text,
@@ -27,9 +33,21 @@ class _SignupPageState extends State<SignupPage> {
       _passwordController.text,
     );
 
+    setState(() {
+      _isLoading = false;
+    });
+
     if (res) {
       Navigator.pushReplacementNamed(context, HomePage.routeName);
     }
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _emailController.dispose();
+    _passwordController.dispose();
+    _usernameController.dispose();
   }
 
   @override
@@ -40,59 +58,61 @@ class _SignupPageState extends State<SignupPage> {
       appBar: AppBar(
         title: const Text('Sign Up'),
       ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 30),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(height: size.height * 0.1),
-              const Text(
-                'Email',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
+      body: _isLoading
+          ? const LoadingIndicator()
+          : SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 30),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(height: size.height * 0.1),
+                    const Text(
+                      'Email',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 8.0),
+                      child: CustomTextField(controller: _emailController),
+                    ),
+                    const SizedBox(height: 20),
+                    const Text(
+                      'Username',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 8.0),
+                      child: CustomTextField(controller: _usernameController),
+                    ),
+                    const SizedBox(height: 20),
+                    const Text(
+                      'Password',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 8.0),
+                      child: CustomTextField(
+                        controller: _passwordController,
+                        passwordVisible: true,
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    CustomButton(
+                      text: 'Sign Up',
+                      onTap: signUpUser,
+                      colorButton: buttonColor,
+                      colortext: Colors.white,
+                    ),
+                  ],
                 ),
               ),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 8.0),
-                child: CustomTextField(controller: _emailController),
-              ),
-              const SizedBox(height: 20),
-              const Text(
-                'Username',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 8.0),
-                child: CustomTextField(controller: _usernameController),
-              ),
-              const SizedBox(height: 20),
-              const Text(
-                'Password',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 8.0),
-                child: CustomTextField(
-                  controller: _passwordController,
-                  passwordVisible: true,
-                ),
-              ),
-              const SizedBox(height: 20),
-              CustomButton(
-                text: 'Sign Up',
-                onTap: signUpUser,
-                colorButton: buttonColor,
-                colortext: Colors.white,
-              ),
-            ],
-          ),
-        ),
-      ),
+            ),
     );
   }
 }
