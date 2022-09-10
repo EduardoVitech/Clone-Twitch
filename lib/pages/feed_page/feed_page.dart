aff8df4bebbc6b8893c7f22b96c677b6e5ea8652
Mizael Eduardo <eduardo_boyfla@hotmail.com>
2022-09-10 14:11:09 -0300

    Designing and displaying feed page

--- lib/pages/feed_page/feed_page.dart
+++ lib/pages/feed_page/feed_page.dart
@@ -1,4 +1,10 @@
+import 'dart:ui';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:twitch_clone/models/livestream/livestream.dart';
+
+import '../../widgets/loading_indicator/loading_indicator.dart';
 
 class FeedPage extends StatefulWidget {
   const FeedPage({super.key});
@@ -10,6 +16,82 @@ class FeedPage extends StatefulWidget {
 class _FeedPageState extends State<FeedPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final size = MediaQuery.of(context).size;
+
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10).copyWith(
+          top: 10,
+        ),
+        child: Column(
+          children: [
+            const Text(
+              'Live User',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 22,
+              ),
+            ),
+            SizedBox(height: size.height * 0.03),
+            StreamBuilder<dynamic>(
+              stream: FirebaseFirestore.instance
+                  .collection('livestream')
+                  .snapshots(),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const LoadingIndicator();
+                }
+
+                return ListView.builder(
+                  itemCount: snapshot.data.docs.length,
+                  itemBuilder: (context, index) {
+                    LiveStream post =
+                        LiveStream.fromMap(snapshot.data.docs[index].data);
+                    return InkWell(
+                      onTap: () {},
+                      child: Container(
+                        height: size.height * 0.1,
+                        margin: const EdgeInsets.symmetric(vertical: 10),
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            AspectRatio(
+                              aspectRatio: 16 / 9,
+                              child: Image.network(post.image),
+                            ),
+                            const SizedBox(width: 10),
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  post.username,
+                                  style: const TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 20,
+                                  ),
+                                ),
+                                Text(
+                                  post.title,
+                                  style: const TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                Text('${post.viewers} watching'),
+                                Text('Started'),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    );
+                  },
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
